00efd821b96558cfad812a83d985ac95a7d21218
Jean-Pierre Bécotte <jp.becotte@me.com>
2022-06-01 16:39:59 -0400

    fix

--- setup-repo.sh
+++ setup-repo.sh
@@ -2,6 +2,6 @@
 url=$(read -p 'Repo URL: ')
 echo $url
 cd /home/dev
-cmd=git clone ${url} && cd $(basename ${url} .git)
+cmd=`git clone ${url} && cd $(basename ${url} .git)`
 echo $cmd
 #su -c "$cmd" dev
